0f365795bdad9fd2263fa2bb131432887a658155
Elshadai <elshadaikassutegegn@gmail.com>
2021-08-18 13:46:46 +0300

    Solve https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

---
+++ sales_by_match.py
@@ -0,0 +1,38 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    result = [0]*101
+    count = 0
+    for i in range(len(ar)):
+        result[ar[i]] += 1
+    for j in range(len(result)):
+        count += result[j] // 2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
