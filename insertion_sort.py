9b68446a2f5040c46fe76b1dc5a953c38144956b
Elshadai <elshadaikassutegegn@gmail.com>
2021-08-18 14:25:04 +0300

    Solve insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+def sorter(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j] :
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+print(sorter([8,2,4,7,9,1,2,5,7,9,4]))
