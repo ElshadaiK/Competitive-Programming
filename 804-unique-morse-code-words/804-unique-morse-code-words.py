5345522ff005f9bcfb9723e7242e511704fdcade
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2022-08-17 18:40:24 -0400

    Time: 62 ms (35.11%), Space: 13.9 MB (75.48%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -0,0 +1,27 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        
+        # create a dictionary for morse code (You can just copy & paste it! ^.^)
+        ENG_to_MORSE = {  
+            'a':".-", 'b':"-...", 'c':"-.-.", 'd':"-..", 'e':".",
+            'f':"..-.", 'g':"--.", 'h':"....", 'i':"..", 'j':".---",
+            'k':"-.-", 'l':".-..", 'm':"--", 'n':"-.", 'o':"---",
+            'p':".--.", 'q':"--.-", 'r':".-.", 's':"...", 't':"-",
+            'u':"..-", 'v':"...-", 'w':".--", 'x':"-..-", 'y':"-.--", 'z':"--..",
+        }
+        
+        cnt = {}    # dictionary for different transformations
+        
+        for word in words:      # loop through every word
+            
+            tmp = ""
+            
+            for c in word:      # loop through every character
+                tmp += ENG_to_MORSE[c]    # convert the word to morse code
+                
+            if tmp not in cnt:
+                cnt[tmp] = 0
+            else:
+                cnt[tmp] += 1
+
+        return len(cnt)    
